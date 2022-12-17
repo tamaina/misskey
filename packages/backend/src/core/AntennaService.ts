06651591d8f4ffb047ec3207cd2fcbfbff748983
tamaina <tamaina@hotmail.co.jp>
2022-12-17 18:15:37 +0000

    Squashed commit of the following:
    commit 112e781e7aac47c49f9760e4a897aa769f5dfbb3
    Author: tamaina <tamaina@hotmail.co.jp>
    Date:   Sat Dec 17 18:13:47 2022 +0000
    
        fix
    
    commit 1d9dc6e69386dfeea911313168e4d2b5b2860456
    Author: tamaina <tamaina@hotmail.co.jp>
    Date:   Sat Dec 17 18:02:50 2022 +0000
    
        wip
    
    commit 7827891c4ed8fcc66ed9e373acfc13a7e7c27480
    Author: tamaina <tamaina@hotmail.co.jp>
    Date:   Sat Dec 17 17:02:34 2022 +0000
    
        wip
    
    commit 70c836766b2966d6a7733847492e5fe651728c38
    Merge: 29aa13df1 4ecc42744
    Author: tamaina <tamaina@hotmail.co.jp>
    Date:   Sat Dec 17 17:00:34 2022 +0000
    
        Merge branch 'develop' into push-notify-antenna
    
    commit 29aa13df10423d3ed387c5550fcb0e1caa323f9a
    Author: tamaina <tamaina@hotmail.co.jp>
    Date:   Sat Dec 17 17:00:08 2022 +0000
    
        wip
    
    commit 4ecc42744c3c8b68e38f58bfe03919bf437f137a
    Author: tamaina <tamaina@hotmail.co.jp>
    Date:   Sun Dec 18 01:59:59 2022 +0900
    
        enhance: Implement the  toggle to (or not to) close push notifications when notifications or messages are read (#9219)
    
        * create file
    
        * wip
    
        * fix
    
        * wip
    
        * tabun dekita
    
        * :v:
    
        * implement subscribe push notification button to tutorial
    
        * check-exists→show-registration
    
        * add column sendReadMessage
    
        * fix migration file
    
        * sw api
    
        * change PushNotificationService
    
        * wip
    
        * :v:
    
        * fix tutorial footer flex

--- packages/backend/src/core/AntennaService.ts
+++ packages/backend/src/core/AntennaService.ts
@@ -3,6 +3,8 @@ import Redis from 'ioredis';
 import type { Antenna } from '@/models/entities/Antenna.js';
 import type { Note } from '@/models/entities/Note.js';
 import type { User } from '@/models/entities/User.js';
+import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
+import { AntennaEntityService } from '@/core/entities/AntennaEntityService.js';
 import { IdService } from '@/core/IdService.js';
 import { isUserRelated } from '@/misc/is-user-related.js';
 import { GlobalEventService } from '@/core/GlobalEventService.js';
