05385f8ba07c13363c24880d15fb78cea4b3bece
tamaina <tamaina@hotmail.co.jp>
2023-04-12 11:56:14 +0000

    in-out?

--- packages/frontend/src/components/MkImgWithBlurhash.vue
+++ packages/frontend/src/components/MkImgWithBlurhash.vue
@@ -2,7 +2,7 @@
 <div :class="[$style.root, { [$style.cover]: cover }]" :title="title ?? ''">
 	<img v-if="!loaded && src" :class="$style.loader" :src="src" @load="onLoad"/>
 	<Transition
-		mode="out-in"
+		mode="in-out"
 		:enter-active-class="props.animation && defaultStore.state.animation ? $style.transition_toggle_enterActive : ''"
 		:leave-active-class="props.animation && defaultStore.state.animation ? $style.transition_toggle_leaveActive : ''"
 		:enter-from-class="props.animation && defaultStore.state.animation ? $style.transition_toggle_enterFrom : ''"
