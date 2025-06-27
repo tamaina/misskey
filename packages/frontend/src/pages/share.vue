d8564e86b1ec70b72354d39ec7090d07fc9663ea
tamaina <tamaina@hotmail.co.jp>
2025-06-28 00:22:51 +0900

    ??

--- packages/frontend/src/pages/share.vue
+++ packages/frontend/src/pages/share.vue
@@ -64,8 +64,10 @@ async function init() {
 
 		//#region add text to note text
 		const duplicateTextRegex = new RegExp(`^${RegExp.escape(title.value)}\\s*`, 'u');
+		console.log('duplicateTextRegex', duplicateTextRegex);
 		if (text?.search(duplicateTextRegex)) {
 			// For the Google app https://github.com/misskey-dev/misskey/issues/16224
+			console.log('Removing duplicate text from note text', text);
 			noteText += text.replace(duplicateTextRegex, '');
 		} else if (text && title.value === text) {
 			// Nothing to do
