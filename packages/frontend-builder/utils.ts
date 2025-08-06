8b83cabc418129df622376d3b4994d7bb3050506
tamaina <tamaina@hotmail.co.jp>
2025-08-07 05:40:47 +0900

    Revert "Merge branch 'pr/anatawa12/16369' into p1-3"
    This reverts commit 3c201d5bb670ef5b9f5493da0938a2dfd24ca891, reversing
    changes made to 3d042e634bf1639cc431567ab30fdd1bb9b767cf.

--- packages/frontend-builder/utils.ts
+++
@@ -1,7 +0,0 @@
-
-export function assertNever(x: never): never {
-	throw new Error(`Unexpected type: ${(x as any)?.type ?? x}`);
-}
-
-export function assertType<T>(node: unknown): asserts node is T {
-}
